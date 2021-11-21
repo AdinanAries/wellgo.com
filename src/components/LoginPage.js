6d4f8184f3557e374597a788052592c7082df572
Mohammed Adinan <adinanaries@outlook.com>
2021-11-21 10:11:10 -0500

    ...

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -11,13 +11,13 @@ export default function LoginPage(){
                             <i style={{marginRight: 10, fontSize: 20, color: "rgba(255,255,255,0.5)"}} className="fa fa-sign-in"></i>
                             Sign In</p>
                         <div style={{padding: "10px",}}>
-                            <div style={{marginBottom: 20}}>
+                            <div style={{marginBottom: 10}}>
                                <div style={{backgroundColor: "rgba(0,0,0,0.07)", border: "none", borderRadius: 50, marginTop: 10, paddingLeft: 14}}>
                                     <i className="fa fa-envelope" style={{marginRight: 10, color: "rgb(43, 52, 61)"}}></i>
                                     <input type="email" placeholder="Email"  style={{padding: 14, paddingLeft: 0, width: "calc(100% - 30px)", background: "none", border: "none"}}/>
                                 </div>
                             </div>
-                            <div style={{marginBottom: 20}}>
+                            <div style={{marginBottom: 10}}>
                                <div style={{backgroundColor: "rgba(0,0,0,0.07)", border: "none", borderRadius: 50, marginTop: 10, paddingLeft: 14}}>
                                     <i className="fa fa-key" style={{marginRight: 10, color: "rgb(43, 52, 61)"}}></i>
                                     <input type="password" placeholder="Password"  style={{padding: 14, paddingLeft: 0, width: "calc(100% - 30px)", background: "none", border: "none"}}/>
@@ -40,25 +40,25 @@ export default function LoginPage(){
                             <i style={{marginRight: 10, fontSize: 20, color: "rgba(255,255,255,0.5)"}} className="fa fa-user"></i>
                             Signup</p>
                         <div style={{padding: "10px",}}>
-                            <div style={{marginBottom: 20}}>
+                            <div style={{marginBottom: 10}}>
                                <div style={{backgroundColor: "rgba(0,0,0,0.07)", border: "none", borderRadius: 50, marginTop: 10, paddingLeft: 14}}>
                                     <i className="fa fa-user" style={{marginRight: 10, color: "rgb(43, 52, 61)"}}></i>
                                     <input type="text" placeholder="Full Name"  style={{padding: 14, paddingLeft: 0, width: "calc(100% - 30px)", background: "none", border: "none"}}/>
                                 </div>
                             </div>
-                            <div style={{marginBottom: 20}}>
+                            <div style={{marginBottom: 10}}>
                                <div style={{backgroundColor: "rgba(0,0,0,0.07)", border: "none", borderRadius: 50, marginTop: 10, paddingLeft: 14}}>
                                     <i className="fa fa-envelope" style={{marginRight: 10, color: "rgb(43, 52, 61)"}}></i>
                                     <input type="email" placeholder="Email"  style={{padding: 14, paddingLeft: 0, width: "calc(100% - 30px)", background: "none", border: "none"}}/>
                                 </div>
                             </div>
-                            <div style={{marginBottom: 20}}>
+                            <div style={{marginBottom: 10}}>
                                <div style={{backgroundColor: "rgba(0,0,0,0.07)", border: "none", borderRadius: 50, marginTop: 10, paddingLeft: 14}}>
                                     <i className="fa fa-key" style={{marginRight: 10, color: "rgb(43, 52, 61)"}}></i>
                                     <input type="password" placeholder="Password"  style={{padding: 14, paddingLeft: 0, width: "calc(100% - 30px)", background: "none", border: "none"}}/>
                                 </div>
                             </div>
-                            <div style={{marginBottom: 20}}>
+                            <div style={{marginBottom: 10}}>
                                <div style={{backgroundColor: "rgba(0,0,0,0.07)", border: "none", borderRadius: 50, marginTop: 10, paddingLeft: 14}}>
                                     <i className="fa fa-key" style={{marginRight: 10, color: "rgb(43, 52, 61)"}}></i>
                                     <input type="password" placeholder="Confirm Password"  style={{padding: 14, paddingLeft: 0, width: "calc(100% - 30px)", background: "none", border: "none"}}/>
