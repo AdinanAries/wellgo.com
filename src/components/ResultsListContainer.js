331c2d290dab74d7dab63161eba41943dbce41a1
Mohammed Adinan <adinanaries@outlook.com>
2021-11-21 10:09:51 -0500

    changed login page form input controls to match overall design

--- src/components/ResultsListContainer.js
+++ src/components/ResultsListContainer.js
@@ -214,7 +214,7 @@ export default function ResultsListContainer(){
                         </div>*/}
                     </div>
                     <div id="animated_loader" style={{position: "relative", height: 190}}>
-                        <div style={{backgroundImage: `url('${airplane}')`, backgroundSize: "contain", backgroundRepeat: "no-repeat", backgroundPosition: "center", width: 120, height: 120, position: "absolute", left: "calc(50% - 80px)", zIndex: 2, animation: "moving_plane 5s ease-in-out infinite"}}></div>
+                        <div style={{backgroundImage: `url('${airplane}')`, backgroundSize: "contain", backgroundRepeat: "no-repeat", backgroundPosition: "center", width: 120, height: 120, position: "absolute", left: "calc(50% - 80px)", zIndex: 2, animation: "the_moving_plane 15s ease-in-out infinite"}}></div>
                     </div>
                     <div style={{animation: "item_slide_down 0.5s ease-in", marginBottom: 20}} className="search_result_inportant_notice_container">
                         <div>
@@ -448,7 +448,8 @@ export default function ResultsListContainer(){
                     </div>
                 </div>
                 <div className="search_list_main_ads_section">
-                    <div style={{height: 500, backgroundImage: `url('${explore_page_hero}')`, backgroundSize: "cover", backgroundRepeat: "no-repeat", backgroundPosition: "center", padding: 20, display: "flex", flexDirection: "column", justifyContent: "center"}}>
+                    <div style={{height: 500, backgroundImage: `url('${explore_page_hero}')`, backgroundSize: "cover", position: "relative", backgroundRepeat: "no-repeat", backgroundPosition: "center", padding: 20, display: "flex", flexDirection: "column", justifyContent: "center"}}>
+                        <p style={{fontSize: 12, backgroundColor: "white", width: 40, textAlign: "center", borderRadius: 20, padding: 5, fontWeight: "bolder", position: "absolute", top: 10, right: 10}}>AD</p>
                         <p style={{color: "white", fontWeight: 1000, fontFamily: "'Prompt', sans-serif", fontSize: 19}}>
                             See the world,</p>
                         <p style={{marginTop: -5,color: "white", fontWeight: 1000, fontFamily: "'Prompt', sans-serif", fontSize: 17 }}>
