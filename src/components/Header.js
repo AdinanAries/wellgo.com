be7dfbdb9fa08bda514887026b88b19bb35cc2eb
Mohammed Adinan <adinanaries@outlook.com>
2021-12-25 14:21:29 -0500

    fixed top search btn

--- src/components/Header.js
+++ src/components/Header.js
@@ -39,7 +39,7 @@ function Header(){
                             </p>
                         </div>
                         <div id="desktop_search_menu_item" onClick={()=>show_home_page(false)} className="each-header-menu-item active">
-                            <div style={{display: "flex", flexDirection: "row", fontSize: 14, padding: "5px", width: 105, border: "2px solid #d66aca", color: "rgba(255,255,255,0.7)", textShadow: "1px 2px 3px rgb(0,0,0,0.33)", borderRadius: 50, backgroundColor: "darkslateblue"}}>
+                            <div style={{display: "flex", flexDirection: "row", fontSize: 14, padding: "5px", width: 105, color: "rgba(255,255,255,0.7)", textShadow: "1px 2px 3px rgb(0,0,0,0.33)", borderRadius: 50, backgroundColor: "darkslateblue", boxShadow: "1px 2px 3px rgba(0,0,0,0.33)"}}>
                                 <div style={{textAlign: "center", display: "flex", flexDirection: "column", justifyContent: "center", width: 30, height: 30, backgroundColor: "white", borderRadius: "100%", marginRight: 10}}>
                                     <i style={{display: 'inline', fontWeight: "initial", color: "rgb(46, 98, 133)", margin: 0, fontSize: 17}} className="fa fa-plane"/>
                                 </div>
@@ -53,7 +53,7 @@ function Header(){
                             <div id="mobile_trips_menu_item" className="header_mobile_menus_item" onClick={show_trips_page}><i className="fa fa-briefcase"></i></div>
                             <div id="mobile_login_menu_item" className="header_mobile_menus_item" onClick={show_login_page}><i className="fa fa-user-circle"></i></div>
                             <div id="mobile_search_menu_item" style={{marginRight: 0}} className="header_mobile_menus_item active" onClick={()=>show_home_page(false)}>
-                                <div style={{display: "flex", flexDirection: "row", margin: 0, fontSize: 14, height: "auto", padding: "5px", width: 95, justifyContent: "flex-start", color: "white", textAlign: "center", textShadow: "1px 2px 3px rgb(0,0,0,0.33)", border: "2px solid #d66aca", color: "rgba(255,255,255,0.7)", textShadow: "1px 2px 3px rgb(0,0,0,0.33)", borderRadius: 50, backgroundColor: "darkslateblue", borderRadius: 50}}>
+                                <div style={{display: "flex", flexDirection: "row", margin: 0, fontSize: 14, height: "auto", padding: "5px", width: 95, justifyContent: "flex-start", color: "white", textAlign: "center", textShadow: "1px 2px 3px rgb(0,0,0,0.33)", boxShadow: "1px 2px 3px rgba(0,0,0,0.33)", color: "rgba(255,255,255,0.7)", textShadow: "1px 2px 3px rgb(0,0,0,0.33)", borderRadius: 50, backgroundColor: "darkslateblue", borderRadius: 50}}>
                                     <div style={{textAlign: "center", display: "flex", height: "auto", flexDirection: "column", justifyContent: "center", width: 26, height: 26, backgroundColor: "white", borderRadius: "100%", margin: 0, marginRight: 10}}>
                                         <i style={{display: 'inline', fontWeight: "initial", color: "rgb(46, 98, 133)", margin: 0, fontSize: 14}} className="fa fa-plane"/>
                                     </div>
