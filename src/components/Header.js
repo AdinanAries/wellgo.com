e0cd845b4bd5361070fd37db54a63bd787b50f12
Mohammed Adinan <adinanaries@outlook.com>
2021-12-25 10:23:35 -0500

    changed top nav search bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -23,12 +23,6 @@ function Header(){
                     </div>
                     
                     <div className="header-menu-items">
-                        <div id="desktop_search_menu_item" onClick={()=>show_home_page(false)} className="each-header-menu-item active">
-                            <p style={{padding: "10px", width: 110, color: "rgba(255,255,255,0.6)"}}>
-                                <i style={{display: 'inline', fontWeight: "initial", color: "rgb(46, 108, 133)"}} className="fa fa-search" />
-                                Search...
-                            </p>
-                        </div>
                         <div id="desktop_explore_menu_item" onClick={show_explore_page} className="each-header-menu-item">
                             <p>
                                 <i className="fa fa-globe" />Explore
@@ -44,16 +38,23 @@ function Header(){
                                 <i className="fa fa-user" />Account
                             </p>
                         </div>
+                        <div id="desktop_search_menu_item" onClick={()=>show_home_page(false)} className="each-header-menu-item active">
+                            <p style={{fontSize: 14, textAlign: "center", padding: "10px", width: 150, color: "rgba(255,255,255)", textShadow: "1px 2px 3px rgb(0,0,0,0.33)", boxShadow: "0 0 10px rgba(0,0,0,0.3)", borderRadius: 9, background: "rgba(0,0,0,0.2)"}}>
+                                <i style={{display: 'inline', fontWeight: "initial", /*color: "rgb(46, 108, 133)"*/ color: "rgba(255,255,255,0.5)"}} className="fa fa-plane" />
+                                Search...
+                            </p>
+                        </div>
                     </div>
                     <div className="header_humburger_menu">
                         <div className="header_mobile_menus" style={{display: "flex", flexDirection: "row"}}>
-                            <div id="mobile_search_menu_item" className="header_mobile_menus_item active" onClick={()=>show_home_page(false)}>
-                            <p style={{fontSize: 15, padding: "5px", width: 85, color: "white"}}>
-                                <i style={{display: 'inline', fontWeight: "initial", fontSize: 18, marginRight: 10}} className="fa fa-search" />
-                                Search...
-                            </p></div>
                             <div id="mobile_trips_menu_item" className="header_mobile_menus_item" onClick={show_trips_page}><i className="fa fa-briefcase"></i></div>
-                            <div id="mobile_login_menu_item" style={{marginRight: 10}} className="header_mobile_menus_item" onClick={show_login_page}><i className="fa fa-user-circle"></i></div>
+                            <div id="mobile_login_menu_item" className="header_mobile_menus_item" onClick={show_login_page}><i className="fa fa-user-circle"></i></div>
+                            <div id="mobile_search_menu_item" style={{marginRight: 5}} className="header_mobile_menus_item active" onClick={()=>show_home_page(false)}>
+                                <p style={{fontSize: 15, padding: "5px", width: 100, color: "white", textAlign: "center", textShadow: "1px 2px 3px rgb(0,0,0,0.33)", boxShadow: "0 0 10px rgba(0,0,0,0.3)", borderRadius: 6, background: "rgba(0,0,0,0.4)"}}>
+                                    <i style={{display: 'inline', fontWeight: "initial", fontSize: 18, marginRight: 10}} className="fa fa-plane" />
+                                    Search...
+                                </p>
+                            </div>
                         </div>
                     </div>
                     
