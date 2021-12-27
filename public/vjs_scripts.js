3de4d0cbdaf6158194fd8b3b67c3744bb913bd86
Mohammed Adinan <adinanaries@outlook.com>
2021-12-27 08:32:58 -0500

    fixing display fixed with 100vh problems

--- public/vjs_scripts.js
+++ public/vjs_scripts.js
@@ -1817,3 +1817,9 @@ window.onscroll = function() {
     }
   }
 }
+const appHeight = () => {
+  const doc = document.documentElement;
+  doc.style.setProperty("--app-height", `${window.innerHeight}px`);
+}
+window.addEventListener("resize", appHeight)
+appHeight();
