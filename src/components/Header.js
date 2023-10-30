2c9472aeaa91be2f53e2b819e15ffc919cba6722
Mohammed Adinan <adinanaries@outlook.com>
2023-10-29 22:39:38 -0400

    changing header search button

--- src/components/Header.js
+++ src/components/Header.js
@@ -42,12 +42,12 @@ function Header(props){
                             </p>
                         </div>
                         <div id="desktop_search_menu_item" onClick={()=>{show_home_page(false);props.show_home_page(true);}} className="each-header-menu-item active">
-                            <div style={{display: "flex", flexDirection: "row", fontSize: 14, padding: "5px", width: 105, color: "rgba(255,255,255,0.7)", textShadow: "1px 2px 3px rgb(0,0,0,0.33)", borderRadius: 50, backgroundColor: "rgb(23, 87, 148)", boxShadow: "0 0 10px rgba(0,0,0,0.4)"}}>
-                                <div style={{textAlign: "center", display: "flex", flexDirection: "column", justifyContent: "center", width: 30, height: 30, backgroundColor: "white", borderRadius: "100%", marginRight: 10}}>
+                            <div style={{display: "flex", flexDirection: "row", fontSize: 14, paddingRight: "5px", width: 105, color: "rgba(255,255,255,0.7)", border: "1px solid rgba(255,255,255,0.2)", borderRadius: 50,}}>
+                                <div style={{textAlign: "center", display: "flex", flexDirection: "column", justifyContent: "center", width: 35, height: 35, backgroundColor: "white", borderRadius: "100%", marginRight: 10}}>
                                     <i style={{display: 'inline', color: "rgb(46, 98, 133)", margin: 0, fontSize: 17}} 
                                         className={showSearchPage?"fa-solid fa-home":"fa-solid fa-plane-departure"}/>
                                 </div>
-                                <div style={{display: "flex", flexDirection: "column", justifyContent: "center", fontSize: 13}}>
+                                <div style={{display: "flex", flexDirection: "column", justifyContent: "center", fontSize: 14, fontFamily: "'Prompt', Sans-serif"}}>
                                     {showSearchPage ? "Home" : "Search"}</div>
                             </div>
                         </div>
@@ -56,12 +56,12 @@ function Header(props){
                         <div className="header_mobile_menus" style={{display: "flex", flexDirection: "row"}}>
                             <div style={{display: "none"}} id="mobile_trips_menu_item" className="header_mobile_menus_item" onClick={show_trips_page}><i className="fa fa-briefcase"></i></div>
                             <div id="mobile_search_menu_item" style={{marginRight: 18}} className="header_mobile_menus_item active" onClick={()=>{show_home_page(false);props.show_home_page(true);}}>
-                                <div style={{display: "flex", flexDirection: "row", margin: 0, fontSize: 14, height: "auto", padding: "5px", width: 100, justifyContent: "flex-start", color: "white", textAlign: "center", textShadow: "1px 2px 3px rgb(0,0,0,0.33)", boxShadow: "0 0 10px rgba(0,0,0,0.33)", color: "rgba(255,255,255,0.7)", textShadow: "1px 2px 3px rgb(0,0,0,0.33)", borderRadius: 50, backgroundColor: "rgb(23, 87, 148)", borderRadius: 50}}>
-                                    <div style={{textAlign: "center", display: "flex", height: "auto", flexDirection: "column", justifyContent: "center", width: 20, height: 20, backgroundColor: "white", borderRadius: "100%", margin: 0, marginRight: 10}}>
+                                <div style={{display: "flex", flexDirection: "row", margin: 0, fontSize: 14, height: "auto", paddingRight: "10px", width: 100, justifyContent: "flex-start", textAlign: "center", color: "rgba(255,255,255,0.7)", border: "1px solid rgba(255,255,255,0.2)", borderRadius: 50}}>
+                                    <div style={{textAlign: "center", display: "flex", flexDirection: "column", justifyContent: "center", width: 35, height: 35, backgroundColor: "white", borderRadius: "100%", margin: 0, marginRight: 10}}>
                                         <i style={{display: 'inline', color: "rgb(46, 98, 133)", margin: 0, fontSize: 15}} 
                                             className={showSearchPage?"fa-solid fa-home":"fa-solid fa-plane-departure"}/>
                                     </div>
-                                    <div style={{display: "flex", flexDirection: "column", justifyContent: "center", height: "auto", margin: 0, fontSize: 13}}>
+                                    <div style={{display: "flex", flexDirection: "column", justifyContent: "center", height: "auto", margin: 0, fontSize: 14,  fontFamily: "'Prompt', Sans-serif"}}>
                                         {showSearchPage ? "Home" : "Search"}
                                     </div>
                                 </div>
