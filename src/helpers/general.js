6ce8acb43896d1542d6f4f61b08d4c7786f94e8f
Mohammed Adinan <adinanaries@outlook.com>
2023-11-15 22:53:46 -0500

    fixing date validation function

--- src/helpers/general.js
+++ src/helpers/general.js
@@ -168,6 +168,10 @@ export const validateYYYYMMDDInputDates = (date_string, date_separator="/") => {
                 }
                 return YYYY+date_separator+MM+date_separator+DD;
             }
+        }else if(lastChar===date_separator && date_string.length===5){
+            return date_string;
+        }else if(lastChar===date_separator && date_string.length===7) {
+            return date_string.substring(0, 4)+date_separator+"0"+date_string.substring(5,6)+date_separator;
         }else{
             return date_string.substring(0, date_string.length-1);
         }
