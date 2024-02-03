3abb55aa1c024ff45e0d89e9c27e438d4915f9be
Mohammed Adinan <adinanaries@outlook.com>
2024-02-03 10:50:49 -0500

    fixing helpers as its own repo

--- src/App.js
+++ src/App.js
@@ -40,6 +40,7 @@ if(localStorage.getItem(CONSTANTS.local_storage.logged_in_usr)){
   localStorage.setItem(CONSTANTS.local_storage.logged_in_usr, AMLOGGEDIN);
 }
 
+// Function to always return the current flight search object in local storage
 window._getSearchObj=()=>{
   return JSON.parse(localStorage.getItem(CONSTANTS.local_storage.flight_search_object));
 }
