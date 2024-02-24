88b3fd124458d8613386a2eaa628f15ec7cf9974
Mohammed Adinan <adinanaries@outlook.com>
2024-02-23 21:16:04 -0500

    ...

--- src/components/FullPageGallery.js
+++ src/components/FullPageGallery.js
@@ -7,7 +7,7 @@ const FullPageGallery = (props) => {
 
     return (
         <div style={{display: "flex", justifyContent: "center", alignItems: "center", backgroundColor: "rgb(43, 52, 61)", position: "fixed", left: 0, top: 0, width: "100vw", height: "100vh", zIndex: 10001}}>
-            <p onClick={toggleShowGallery} style={{width: 33, height: 33, display: "flex", justifyContent: "center", alignItems: "center", borderRadius: "100%", background: "crimson", color: "white", position: "absolute", top: 10, right: 20, cursor: "pointer"}}>
+            <p onClick={toggleShowGallery} style={{width: 33, height: 33, display: "flex", justifyContent: "center", alignItems: "center", borderRadius: "100%", background: "crimson", color: "white", position: "absolute", top: 10, right: 20, cursor: "pointer", zIndex: 10002}}>
                 <i style={{fontSize: 12}} className="fa-solid fa-times"></i></p>
             <div style={{width: "100%", maxWidth: "900px"}}>
                 <Carousel >
